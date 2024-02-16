1b5e2aeab1c5f84b86ec9fd272874c3c53b3df9c
Mario G. Valle <mariogvalle@gmail.com>
2024-02-15 23:54:54 -0600

    Avance 2

---
+++ app/src/main/java/com/examen/pm2e17167/ActivityLlamadas.java
@@ -0,0 +1,14 @@
+package com.examen.pm2e17167;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLlamadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llamadas);
+    }
+}
